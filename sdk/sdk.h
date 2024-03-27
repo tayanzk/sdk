cc5b329d31626d0f68e895ea5d67b977e21337bb
tayanzk <77182183+tayanzk@users.noreply.github.com>
2024-03-26 23:38:25 -0700

    sdk

---
+++ sdk/sdk.h
@@ -0,0 +1,15 @@
+#ifndef _SDK_h
+#define _SDK_h 1
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#define min(a, b) (a > b) ? (b) : (a)
+#define max(a, b) (a > b) ? (a) : (b)
+
+typedef unsigned int uint;
+
+#endif
