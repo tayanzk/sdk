42e22f2b22d9bb69317946b6eaaaa6eca18ee6c2
tayanzk <77182183+tayanzk@users.noreply.github.com>
2024-04-06 20:37:41 -0700

    move definitions

--- sdk/hash.h
+++ sdk/hash.h
@@ -3,21 +3,7 @@
 
 #include <sdk/sdk.h>
 
-u64 hash_pointer(char *ptr, u64 size)
-{
-  assert(size == 8);
-
-  return (u64) ptr;
-}
-
-u64 hash_fnv64a(char *ptr, u64 size)
-{
-  u64 hash = 0xcbf29ce484222325;
-
-  for (u32 i = 0; i < size; i++)
-    hash = (hash ^ ptr[i]) * 0x100000001b3;
-
-  return hash;
-}
+u64 hash_pointer(char *ptr, u64 size);
+u64 hash_fnv64a(char *ptr, u64 size);
 
 #endif
