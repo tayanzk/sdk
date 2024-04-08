97741655f066a8efbe996c22049d05b3c350e986
tayanzk <77182183+tayanzk@users.noreply.github.com>
2024-04-08 12:31:29 -0700

    pushing/poping arrays

--- sdk/buf.c
+++ sdk/buf.c
@@ -3,9 +3,9 @@
 
 static const uint _Grow = 8; // elements
 
-static inline bool grow(buf_t *buffer)
+static inline bool grow(buf_t *buffer, uint count)
 {
-  uint new = buffer->size + buffer->stride * _Grow;
+  uint new = buffer->size + buffer->stride * count;
   uint len = buffer->top - buffer->data;
 
   void *ptr = reallocate(buffer->allocator, buffer->data, buffer->size, new);
@@ -56,31 +56,65 @@ void buf_delete(buf_t *buffer)
   deallocate(buffer->allocator, buffer);
 }
 
+static inline uint pushs(buf_t *buffer, const void *src, uint size)
+{
+  char *bend = buffer->data + buffer->size;
+  char *pend = buffer->top + size;
+
+  if (pend >= bend)
+  {
+    uint count = max((pend - bend + size) / buffer->stride, _Grow);
+
+    if (!grow(buffer, count))
+      return 0;
+  }
+
+  memcpy(buffer->top, src, size);
+  buffer->top += size;
+
+  return buffer->top - buffer->data;
+}
+
+uint buf_pushc(buf_t *buffer, const void *src, uint count)
+{
+  assert(buffer && buffer->data && src);
+
+  return pushs(buffer, src, buffer->stride * count);
+}
+
 uint buf_push(buf_t *buffer, const void *src)
 {
   assert(buffer && buffer->data && src);
 
-  if (buf_full(buffer))
-    if (!grow(buffer))
-      return 0;
+  return pushs(buffer, src, buffer->stride);
+}
 
-  memcpy(buffer->top, src, buffer->stride);
-  buffer->top += buffer->stride;
+static inline uint pops(buf_t *buffer, void *dst, uint size)
+{
+  if (buffer->top == buffer->data)
+    return 0;
+
+  if (buffer->top - size < buffer->data)
+    return 0;
+
+  buffer->top -= size;
+  memcpy(dst, buffer->top, size);
 
   return buffer->top - buffer->data;
+}
+
+uint buf_popc(buf_t *buffer, void *dst, uint count)
+{
+  assert(buffer && buffer->data && dst);
+
+  return pops(buffer, dst, buffer->stride * count);
 }
 
 uint buf_pop(buf_t *buffer, void *dst)
 {
   assert(buffer && buffer->data && dst);
 
-  if (buf_empty(buffer))
-    return 0;
-
-  buffer->top -= buffer->stride;
-  memcpy(dst, buffer->top, buffer->stride);
-
-  return buffer->top - buffer->data;
+  return pops(buffer, dst, buffer->stride);
 }
 
 void *buf_next(buf_t *buffer)
--- sdk/buf.h
+++ sdk/buf.h
@@ -15,7 +15,9 @@ typedef struct buf_t
 buf_t *buf_create(uint reserve, uint stride, allocator_t *allocator);
 void buf_delete(buf_t *buffer);
 
+uint buf_pushc(buf_t *buffer, const void *src, uint count);
 uint buf_push(buf_t *buffer, const void *src);
+uint buf_popc(buf_t *buffer, void *dst, uint count);
 uint buf_pop(buf_t *buffer, void *dst);
 void *buf_next(buf_t *buffer);
 
